78578f3904a6dd086691586bf7b5f2ecb5f14a6d
MorohaZ <141795069+MorohaZ@users.noreply.github.com>
2024-09-04 16:24:01 +0700

    Update page.tsx

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,41 +1,43 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import MovieRow from "@components/MovieRow";
+import { movieDB } from "@lib/movieDB";
+export default function SearchResultPage({ params }: any) {
+  //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
+  // const processedSearchInput = ...
+  const movieName = params.searchInput.replaceAll("%20"," ");
+  const selMovie = movieDB.filter((movie) => movie.title.toLowerCase().includes(movieName.toLowerCase()));
+  if (!selMovie) {
+    return (<p className="text-center">{"Movie is not found 😥 "+ movieName}</p>);
+  }
+  /*
+  tip2 : Use "includes" string method to check substring
+  Example : "ABC".includes("AB") -> return true
 
-export default function SearchPage() {
-  const [searchInput, setSearchInput] = useState("");
-
-  //A hook that helps navigating each route programmatically
-  const router = useRouter();
-
-  const searchInputOnChange = (event) => {
-    setSearchInput(event.target.value);
-  };
-
-  const searchInputOnKeyUp = (event) => {
-    if (event.key === "Enter") searchBtnOnClick();
-  };
-
-  const searchBtnOnClick = () => {
-    router.push("/search/" + searchInput);
-  };
+  tip3 : To implement case insensitive searching, use "toLocaleLowerCase" string method
+  to convert movie title and searchInput to lower case 
+  const filteredMovies = movieDB.filter((movie) =>
+    you code here...
+  );
+  */
 
   return (
     <div>
-      <p className="fw-bold fs-4 text-center">Search Movie</p>
-      <div className="d-flex gap-2">
-        <input
-          className="form-control"
-          placeholder="Search movie..."
-          onChange={searchInputOnChange}
-          onKeyUp={searchInputOnKeyUp}
-          value={searchInput}
-        />
-        <button className="btn btn-primary" onClick={searchBtnOnClick}>
-          Search
-        </button>
-      </div>
+      <p className="fw-bold fs-4 text-center my-0">
+        Searching &quot; {movieName} &quot;
+      </p>
+      <p className="fw-bold fs-4 text-center">Found {selMovie.length} result(s)</p>
+      {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      {selMovie.map((movie, i) => (
+          <MovieRow
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            detail={movie.detail}
+            rating={movie.rating}
+            number={i + 1}
+          />
+        ))}
     </div>
   );
 }
